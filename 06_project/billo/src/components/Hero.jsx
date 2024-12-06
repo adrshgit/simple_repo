b04dcda8330b9772fe079ae08ba14d91d5b77a7a
AD₹SH <140497925+codexadarsh@users.noreply.github.com>
2024-12-07 00:57:50 +0530

    new project

---
+++ 06_project/billo/src/components/Hero.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className='content'>
+      <div className="left">
+        <h1>Invoice Generator App </h1>
+        <p>Genrate the invoices of your customers easily and quickly</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum atque iusto sit autem delectus perspiciatis aliquam iure voluptate quia recusandae molestias ratione minima, inventore a, aliquid officiis! Soluta, provident quasi.</p>
+        <button>Get Started</button>
+      </div>
+      <div className="right">
+        <img src="/src/assets/invoice.png" alt="" />
+      </div>
+
+    </div>
+  )
+}
+
+export default Hero
---
+++ 06_project/billo/src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+        <div className="nav">
+            <a href="#">Geninvo</a>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Contact</a></li>
+                <li><a href="#">Services</a></li>
+            </ul>
+            <button>Get Started</button>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
